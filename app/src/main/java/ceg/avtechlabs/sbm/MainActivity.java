5604cb8c366125b054780fca33375ca06dec4029
v.adhithyan <adhithyan-3592@adhithyan-3592.wirelessap>
2016-01-30 07:31:48 +0530

    splash to be shown in full screen

--- app/src/main/java/ceg/avtechlabs/sbm/MainActivity.java
+++ app/src/main/java/ceg/avtechlabs/sbm/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 import static ceg.avtechlabs.sbm.R.string.app_name;
 
@@ -21,8 +23,14 @@ public class MainActivity extends ActionBarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_splash_screen);
+
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.actionBarColor)));
+
         new Handler().postDelayed(new Runnable()
         {
             @Override
