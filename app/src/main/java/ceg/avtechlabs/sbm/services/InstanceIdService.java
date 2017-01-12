a5bd1689566a99b870d79bda2509dc759011133a
v.adhithyan <aavispeaks@gmail.com>
2017-01-12 08:00:40 +0530

    FCM integration tested

--- app/src/main/java/ceg/avtechlabs/sbm/services/InstanceIdService.java
+++ app/src/main/java/ceg/avtechlabs/sbm/services/InstanceIdService.java
@@ -9,6 +9,7 @@ import com.google.firebase.iid.FirebaseInstanceIdService;
  * Created by adhithyan-3592 on 30/10/16.
  */
 
+// cyD63DlMZyU:APA91bFTAv-I2lYS-d6eugnTomJlRIgaUKMJtiqueIfTnKGOpvqMsXyZxinztyr4XWy7hRV0qPnmBTv2fUL7kzvBDoUUNFi-I0iMnZlakXzXNCjUdgQPqgLJZy3SuFoUF_XzRxSRnHHH
 public class InstanceIdService extends FirebaseInstanceIdService {
     static final String TAG = "sbm-log";
 
